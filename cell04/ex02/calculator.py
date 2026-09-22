46dedaf7b66ce1f8351886c15bd2f1eb989da4f3
Gubally <komkhum41586@gmail.com>
2026-09-22 10:55:23 +0700

    Add basic calculator functionality in calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+first_number = input("Give me the first number: ")
+first_number = int(first_number)
+
+second_number = input("Give me the second number: ")
+second_number = int(second_number)
+
+print("Thank you!")
+print(str(first_number) + " + " + str(second_number) + " = " + str(first_number + second_number))
+print(str(first_number) + " - " + str(second_number) + " = " + str(first_number - second_number))
+print(str(first_number) + " / " + str(second_number) + " = " + str(first_number / second_number))
+print(str(first_number) + " * " + str(second_number) + " = " + str(first_number * second_number))
