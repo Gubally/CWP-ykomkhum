7351bbaf09af9de209ea446dda0e97ae77fb4dd2
Gubally <komkhum41586@gmail.com>
2026-09-22 09:29:20 +0700

    Add iszero.py to check if a number is zero

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+number = input("Number: ")
+number = int(number)
+
+if number == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
