e6305a8a6e55b2edd42becc22d7092964a12f45d
Gubally <komkhum41586@gmail.com>
2026-09-22 09:29:41 +0700

    Delete cell00/ex00/iszero.py

--- cell00/ex00/iszero.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-number = input("Number: ")
-number = int(number)
-
-if number == 0:
-    print("This number is equal to zero.")
-else:
-    print("This number is different from zero.")
