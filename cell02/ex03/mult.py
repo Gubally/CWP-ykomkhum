77b77dee6c8c7bc9de435b00b81dc8274141ad5e
Gubally <komkhum41586@gmail.com>
2026-09-22 09:44:55 +0700

    Add multiplication script with result evaluation

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+first_number = input("Enter the first number:\n")
+first_number = int(first_number)
+
+second_number = input("Enter the second number:\n")
+second_number = int(second_number)
+
+result = first_number * second_number
+
+print(str(first_number) + " x " + str(second_number) + " = " + str(result))
+
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is zero.")
