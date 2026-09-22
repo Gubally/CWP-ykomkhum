cfc4193ac6429493c4686099d259c67e49cb589c
Gubally <komkhum41586@gmail.com>
2026-09-22 09:26:48 +0700

    Add password verification script

---
+++ cell02/ex02/password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+password = "Python is awesome"
+
+entered_password = input("Password: ")
+
+if entered_password == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
